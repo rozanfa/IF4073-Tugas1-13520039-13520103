caf8f359411cdeeb7be6837e8c5115dad24ce27c
Rozan Fadhil Al Hafidz <rozanfadhilalhafidz@gmail.com>
2023-09-19 22:43:12 +0700

    fix histogram_map; add contrast_stretch

--- hist.m
+++ hist.m
@@ -1,24 +1,20 @@
 
-
 image = imread('Lena.bmp');
 image_size = size(image);
 
 dimension = ndims(image);
 if dimension == 2
-    image_size = [1 image_size];
+    image_size = [image_size 1];
 end
 
-histogram_map = zeros(image_size(1), 255, "uint8");
+histogram_map = zeros(image_size(1), 256, "uint32");
 
 for i = 1:image_size(1)
     for j = 1:image_size(2)
         for k = 1:image_size(3)
-            histogram_map(i, image(k, j, i)) = histogram_map(i, image(k, j, i)) + 1;
+            histogram_map(i, image(k, j, i)+1) = histogram_map(i, image(k, j, i)+1) + 1;
         end
     end
 end
 
-disp(histogram_map)
 bar(histogram_map);
-
-
